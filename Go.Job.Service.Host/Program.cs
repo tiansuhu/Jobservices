a8af0e69d49e066bc42c7b03abce42b7163b47e8
tiansuhu <123456@qq.com>
2019-11-15 11:19:30 +0800

    添加基础版本

---
+++ Go.Job.Service.Host/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Go.Job.Model;
+using Go.Job.Service.Api;
+
+namespace Go.Job.Service.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           JobServiceBuilder.BuilderProduce().Start();
+
+            JobApiStartHelper.Start("wechat");
+
+
+
+            //JobServiceBuilder.BuilderTest().Start(new JobInfo
+            //{
+            //    AssemblyPath = @"C:\Users\h3user\Downloads\Go.Job.QuartzNET3X-master\Go.Job.QuartzNET3X-master\TestJob1\bin\Debug\TestJob1.dll",
+            //    ClassType = "TestJob1.Job1",
+            //    Second = 1,
+            //});
+
+            //JobServiceBuilder.BuilderTest().Start();
+        }
+    }
+}
---
+++ Go.Job.Service.Logic/Listener/DefaultTriggerListener.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Listener;
+
+namespace Go.Job.Service.Logic.Listener
+{
+    /// <summary>
+    /// 触发器监听器基类
+    /// </summary>
+    public sealed class DefaultTriggerListener : TriggerListenerSupport
+    {
+
+        public Action<IJobExecutionContext, ITrigger> FiredAction;
+
+        public Action<IJobExecutionContext, ITrigger> CompleteAction;
+
+        public Action<ITrigger> MisFiredAction;
+
+        public Action<ITrigger> VetoJobAction;
+
+        public override string Name { get; } = "Default";
+
+
+        public override Task TriggerFired(ITrigger trigger, IJobExecutionContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            FiredAction?.Invoke(context, trigger);
+            return base.TriggerFired(trigger, context, cancellationToken);
+        }
+
+        public override Task TriggerComplete(ITrigger trigger, IJobExecutionContext context, SchedulerInstruction triggerInstructionCode, CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            CompleteAction?.Invoke(context, trigger);
+
+            return base.TriggerComplete(trigger, context, triggerInstructionCode, cancellationToken);
+        }
+
+        public override Task TriggerMisfired(ITrigger trigger, CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            MisFiredAction?.Invoke(trigger);
+
+            return base.TriggerMisfired(trigger, cancellationToken);
+        }
+
+        public override Task<bool> VetoJobExecution(ITrigger trigger, IJobExecutionContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            VetoJobAction?.Invoke(trigger);
+
+            return base.VetoJobExecution(trigger, context, cancellationToken);
+        }
+    }
+}
